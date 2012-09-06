cb7dcfd97e026bc0875efd012cf1f07613ab2a37
clampist <clampist@gmail.com>
2012-09-06 17:25:22 +0800

    add notify

--- xiami_auto_checkin.py
+++ xiami_auto_checkin.py
@@ -3,9 +3,9 @@
 # +-----------------------------------------------------------------------------
 # | File: xiami_auto_checkin.py
 # | Author: clampist
-# | E-mail: clampist[at]gmail.com
+# | E-mail: clampist[at]gmail[dot]com
 # | Created: 2012-06-23
-# | Last modified: 2012-06-23
+# | Last modified: 2012-09-06
 # | Description:
 # |     auto checkin for xiami.com
 # | Forked from: huxuan/xiami_auto_checkin
@@ -20,6 +20,7 @@ import urllib
 import urllib2
 import datetime
 import cookielib
+import subprocess
 
 def check(response):
     """Check whether checkin is successful
@@ -50,10 +51,10 @@ def main():
 
     # Get email and password
     if len(sys.argv) != 3:
+        print '[Error] Please input email & password as sys.argv!'
+        subprocess.call(['notify-send', '[Error] Please input email & password as sys.argv!'])
         print >>f, '[Error] Please input email & password as sys.argv!'
-        print '[Usage] $ python2 xiami_auto_checkin.py EMAIL PASSWORD'
         print >>f, datetime.datetime.now()
-        print datetime.datetime.now()
         return
     email = sys.argv[1]
     password = sys.argv[2]
@@ -76,13 +77,14 @@ def main():
         # Checkin Already | Login Failed
         result = check(login_response)
         if result:
-            print >>f, '[Succeed] Checkin Already!', email, result
-            print '[Succeed] Checkin Already!', email, result
+            print '[Already] Checkin Already!', email, result
+            subprocess.call(['notify-send', '[Already] Checkin Already!', email +' '+ result])
+            print >>f, '[Already] Checkin Already!', email, result
         else:
-            print >>f, '[Error] Login Failed!', email
             print '[Error] Login Failed!', email
+            subprocess.call(['notify-send', '[Error] Login Failed!', email])
+            print >>f, '[Error] Login Failed!', email
         print >>f, datetime.datetime.now()
-        print datetime.datetime.now()
         return
     checkin_url = 'http://www.xiami.com' + checkin_result.group(1)
     checkin_headers = {'Referer':'http://www.xiami.com/web', 'User-Agent':'Opera/9.60',}
@@ -92,13 +94,14 @@ def main():
     # Result
     result = check(checkin_response)
     if result:
-        print >>f, '[Succeed] Checkin Succeed!', email, result
-        print '[Succeed] Checkin Succeed!', email, result
+        print '[Success] Checkin Succeed!', email, result
+        subprocess.call(['notify-send', '[Success] Checkin Succeed!', email +' '+ result])
+        print >>f, '[Success] Checkin Succeed!', email, result
     else:
-        print >>f, '[Error] Checkin Failed!'
         print '[Error] Checkin Failed!'
+        subprocess.call(['notify-send', '[Error] Checkin Failed!'])
+        print >>f, '[Error] Checkin Failed!'
     print >>f, datetime.datetime.now()
-    print datetime.datetime.now()
 
 if __name__=='__main__':
     main()
